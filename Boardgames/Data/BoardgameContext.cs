b56473b4e8f76b4b7764feaa1de69a4ad9e07362
MatasSuslavicius <79166862+MatasSuslavicius@users.noreply.github.com>
2022-12-13 22:55:54 +0200

    deployment

--- Boardgames/Data/BoardgameContext.cs
+++ Boardgames/Data/BoardgameContext.cs
@@ -10,12 +10,15 @@ namespace Boardgames.Data
         public DbSet<Game> Games { get; set; }
         public DbSet<Ad> Ads { get; set; }
         public DbSet<Question> Questions { get; set; }
-
+        private readonly IConfiguration configuration;
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             // !!! DON'T STORE THE REAL CONNECTION STRING THE IN PUBLIC REPO !!!
             // Use secret managers provided by your chosen cloud provider
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=Boardgames");
+
+            //optionsBuilder.UseSqlServer(configuration.GetConnectionString("DBConnection"));
+            //optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=Boardgames");
+            optionsBuilder.UseSqlServer("Data Source=tcp:boardgamesdbserver.database.windows.net,1433;Initial Catalog=Boardgames_db;User Id=matsus@boardgamesdbserver;Password=root.123");
         }
     }
 }
